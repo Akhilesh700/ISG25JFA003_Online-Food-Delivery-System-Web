06c21694b1e0f6e6ba4dc600533d975139f787dd
Singh <2441326@cognizant.com>
2025-10-29 09:52:48 +0530

    Added: Added landing page routing

--- src/app/features/customer/landing/signup/signup.ts
+++ src/app/features/customer/landing/signup/signup.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
@@ -12,21 +13,22 @@ export class Signup {
         imgUrl: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=3087&auto=format&fit=crop", 
         tagline: "Order Your Favorite Food", 
         description: "Get delicious meals delivered to your doorstep from the best local restaurants. Quick, easy, and convenient.", 
-        button: "Sign Up to Order"
-
+        button: "Sign Up to Order",
+        url: "auth/signup"
       },
       {
         imgUrl: "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2940&auto=format&fit=crop", 
         tagline: "Partner with Us", 
         description: "Grow your business by reaching new customers in your area. Join our network of restaurant partners today.", 
-        button: "Add Your Restaurant"
-
+        button: "Add Your Restaurant",
+        url: "auth/signup/restaurant"
       },
       {
         imgUrl: "img/Delivery_Agent.png", 
         tagline: "Become a Rider", 
         description: "Earn money on your own schedule. Deliver with Dine Cognizant and enjoy the flexibility of being your own boss.", 
-        button: "Sign Up to Deliver"
+        button: "Sign Up to Deliver",
+        url: "auth/signup/delivery-agent"        
       }
     ]
 }
