6f0644cdb3b0745b5e13b9c4bddda0b24e609e53
Sankalp <sankalpbhagwan.salve@cognizant.com>
2025-10-29 11:19:16 +0530

    Fixed: theme error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -38,6 +38,11 @@ export const routes: Routes = [
         path: 'session-expired',
         loadComponent : () => import('./shared/components/session-expired/session-expired.component').then(m=>m.SessionExpiredComponent)
     },
+    //Todo: Remove it later
+    {
+        path: 'restaurant/dashboard',
+        redirectTo: 'session-expired'
+    },
     // Wildcard Route (Fallback):
     // If the user navigates to any URL that doesn't match the routes defined above,
     // this will redirect them back to the home page.
